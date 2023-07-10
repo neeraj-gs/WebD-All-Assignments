542b6863828e86806fe137a29e79bb872d0a6934
neeraj-gs <gsneeraj05@gmail.com>
2023-07-10 11:36:37 +0530

    Started with the 2nd part of the week3 assignemnt creating a course selling website -easy part

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const port = 3000;
 
 app.use(express.json());
 
@@ -10,6 +12,7 @@ let COURSES = [];
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+
 });
 
 app.post('/admin/login', (req, res) => {
