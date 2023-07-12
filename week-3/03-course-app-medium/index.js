2acf134c8e03a65af3e7341789205d26844bf7a6
neeraj-gs <gsneeraj05@gmail.com>
2023-07-12 11:51:17 +0530

    Started Solving 3rd assignmetn of week3 where we store all the data or responses in a file and not in memory

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 let ADMINS = [];
 let USERS = [];
@@ -49,6 +52,6 @@ app.get('/users/purchasedCourses', (req, res) => {
   // logic to view purchased courses
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is listening on port 3000');
 });
