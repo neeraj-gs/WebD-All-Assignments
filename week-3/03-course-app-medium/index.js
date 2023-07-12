3908a5f4d5eb20dfbe967cb297d80b39f791217f
neeraj-gs <gsneeraj05@gmail.com>
2023-07-12 11:52:35 +0530

    Installed jwt for authentication and updated dependencies of the 3rd assignemnt

--- week-3/03-course-app-medium/index.js
+++ week-3/03-course-app-medium/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
+const jwt = require('jsonwebtoken')
 
 app.use(express.json());
 app.use(bodyParser.json());
